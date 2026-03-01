99e5a6c702d79261f54a3b971f50fea3e8b389d3
opeyemibioku <opeyemibioku@gmail.com>
2026-03-01 23:39:20 +0100

    updated the email provider

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,59 +1,10 @@
-// import nodemailer from "nodemailer";
-
-// const contactEmail = async (options) => {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USERNAME,
-//       pass: process.env.EMAIL_PASSWORD,
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: '"Jolevi" <jolevi.tech@gmail.com>',
-//     // from: "Jolevi",
-//     to: options.email,
-//     subject: options.subject,
-//     text: options.message,
-//     html: options.html,
-//   };
-
-//   await transporter.sendMail(mailOptions);
-// };
-
-// async function sendContactEmail(name, email) {
-//   try {
-//     await contactEmail({
-//       email: email,
-//       subject: "Thank you for contacting Jolevi",
-//       //   message: `Click this link to confirm your email: ${confirmationUrl}`,
-//       html: `
-//       <p>Hello there,</p>
-//       <p>Thank you for reaching out to us at Jolevi. You are sure to hear from us within the next 2 working days.</p>
-//       <p>Please know that we value and appreciate you.</p>
-//       <p>Talk to you soon</p>
-//       `,
-//     });
-
-//     // Return true to indicate that the email was successfully sent
-//     return true;
-//   } catch (error) {
-//     console.error("Email sending error:", error);
-
-//     // Return false to indicate that there was an error sending the email
-//     return false;
-//   }
-// }
-
-// export default sendContactEmail;
-
 import { Resend } from "resend";
 
 async function sendUserConfirmationEmail(name, email) {
   try {
     const resend = new Resend(process.env.RESEND_API_KEY); // ✅ inside function
     await resend.emails.send({
-      from: "Jolevi <noreply@yourdomain.com>",
+      from: "Jolevi <noreply@jolevi.com>",
       reply_to: process.env.ADMIN_EMAIL,
       to: email,
       subject: "Thank you for contacting Jolevi",
